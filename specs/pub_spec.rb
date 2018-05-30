62e8cd817a1e283e63a1a8ff87d71342c71e8583
Gary Muir <garymuir17@gmail.com>
2018-05-30 11:49:39 +0100

    pub_spec setup done

--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -4,4 +4,16 @@ require_relative("../pub.rb")
 
 class PubTest < MiniTest::Test
 
+  def setup
+    @drink1 = Drink.new(1,200,"beer")
+    @drink2 = Drink.new(2,200,"beer")
+
+    @pub1 = Pub.new("Chanter")
+  end
+
+  def test_pub_has_name
+    assert_equal("Chanter", @pub1.name())
+  end
+
+
 end
