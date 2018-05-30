956b1a9e195bd84d2babd699c791d6000658d26d
Gary Muir <garymuir17@gmail.com>
2018-05-30 11:34:59 +0100

    setup drinks_spec

--- specs/drink_spec.rb
+++ specs/drink_spec.rb
@@ -1,6 +1,14 @@
-require ("minitest/autorun")
-require_relative ("../drink.rd")
+require("minitest/autorun")
+require_relative("../drink.rb")
 
 class DrinkTest < MiniTest::Test
-  
+
+  def setup
+    @drink1 = Drink.new(1,200,"beer")
+  end
+
+  def test_drink_has_name
+    assert_equal("beer", @drink1.type())
+  end
+
 end
