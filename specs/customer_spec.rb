119c16e041f0694db7a264b8dba18c70f3c1008a
Gary Muir <garymuir17@gmail.com>
2018-05-30 13:54:42 +0100

    setup customer_spec.rb

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -5,4 +5,18 @@ require_relative("../pub.rb")
 
 class CustomerTest < MiniTest::Test
 
+  def setup
+    drink1 = Drink.new(1,200,"beer")
+    drink2 = Drink.new(2,200,"beer")
+
+    @pub1 = Pub.new("Chanter")
+    drinks = [drink1, drink2]
+    @pub1.add_many_drinks(drinks)
+    @customer1 = Customer.new("Gary", 20)
+  end
+
+  def test_customer_has_name
+    assert_equal("Gary", @customer1.name())
+  end
+
 end
